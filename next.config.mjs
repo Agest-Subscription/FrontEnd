2ee793e169be5d5e0ea67aaa692b3d2fadd4c226
Quocc1 <caobinhoh@gmail.com>
2024-07-08 11:24:12 +0700

    Minor update

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    compiler: {
-        styledComponents: true
-    }
+  output: "standalone",
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 export default nextConfig;
