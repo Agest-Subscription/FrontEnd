b87ed98c659108a58b309c6e2d4dbddfc9db9b24
Viznem <thinhtruong3344@gmail.com>
2024-07-08 14:59:47 +0700

    add animation for popup

--- src/components/popup/Popup.tsx
+++ src/components/popup/Popup.tsx
@@ -1,6 +1,9 @@
 import { popup } from "@/interfaces/popup";
+import { useSpring, animated, useTransition } from '@react-spring/web';
 import React from "react";
 import './Popup.css'
+import { useEffect } from "react";
+import { size } from "lodash";
 
 
 type Props = {
@@ -10,18 +13,51 @@ type Props = {
 };
 
 const PopUp = ({ popupProps, isOpen = false, onClose }: Props) => {
-    return (isOpen && (
-        <div className="react-modal-overlay">
-            <div className="react-modal-wrapper">
-                <div className="react-modal-content">
-                    {popupProps.popup_text}
-                    <button type="button" onClick={onClose}>Close</button>
-                </div>
-            </div>
-        </div>
-    )
+   
+    const handleEscape = (e: KeyboardEvent) => {
+        if(e.keyCode === 27){
+            onClose();
+        }
+    }
 
-    )
+    useEffect(() => {
+       return () => document.addEventListener('keydown', handleEscape);
+    })
+
+    const modalTransition = useTransition(isOpen, {
+        from: { opacity: 0},
+        enter: { opacity: 1},
+        leave: { opacity: 1},
+        config: {
+            duration: 300
+        }
+    })
+    const springs = useSpring({
+        opacity: isOpen ? 1 : 0,
+        transform: isOpen ? "translateY(0%)" : "translateY(-100%)",
+        config: {
+           duration: 300 
+        }
+    })
+    
+    return modalTransition((styles, isOpen) => isOpen && (
+        <animated.div style={styles} className="react-modal-overlay">
+            <animated.div style={springs} className="react-modal-wrapper">
+                <div className="react-modal-content">
+                    <div className="react-modal-header">
+                        <h2>{popupProps.popup_type}</h2>
+                        <button onClick={onClose} className="close" aria-label="Close modal"></button>
+                    </div>
+                    <div className="react-modal-body">
+                        {popupProps.popup_text}
+                    </div>
+                    <div className="react-modal-footer">
+                        <button type="button" onClick={onClose}>Close</button>
+                    </div>
+                </div>
+            </animated.div>
+        </animated.div>
+    ))
 }
 
 export default PopUp;
