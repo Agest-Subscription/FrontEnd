236b209823da0c4ac9bf31cec3cc7a4cc1b0943d
quangdethuong <quangnn1106@gmail.com>
2024-07-18 16:13:16 +0700

    feat: SS-63 Admin Login flow, protected routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+
+import { PERMISSIONS } from "@/constants/routes";
 import LoginContainer from "@/containers/login/Login";
 
 export default function Home() {
+  const { status } = useSession();
+  const router = useRouter();
+
+  if (status === "authenticated") {
+    return router.push(PERMISSIONS);
+  }
   return (
     <>
       <LoginContainer />
