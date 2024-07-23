c7f31e652c959608d045aa4a19e707518074db83
Viznem <thinhtruong3344@gmail.com>
2024-07-23 15:11:43 +0700

    Add landing page static for demo

---
+++ src/app/landing-page/page.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import React, { useState } from "react";
+import PricingCard from "@/components/pricing/PricingCard";
+import { DummyData } from "./DummyData";
+import type { RadioChangeEvent } from 'antd';
+import { Flex, Radio, Typography } from "antd";
+
+
+
+const LandingPage = () => {
+  const [value, setValue] = useState('Weekly');
+  const Options = [
+    { label: 'Weekly', value: 'Weekly' },
+    { label: 'Monthly', value: 'Monthly' },
+    { label: 'Yearly', value: 'Yearly'},
+  ];
+
+  const onChange = ({ target: { value } }: RadioChangeEvent) => {
+    console.log('radio checked', value);
+    setValue(value);
+  };
+
+  return (
+  <div>
+    <Flex vertical align="center">
+    <Typography
+        style={{
+          fontSize: 50,
+          fontWeight: 700,
+          color: "black",
+        }}
+      >
+        Pricing
+    </Typography>
+    <Typography style={{ fontSize: 16, fontWeight: 400 }}>
+        Officia exercitation quis voluptate elit consequat nostrud
+    </Typography>
+
+    <Radio.Group options={Options} value={value} onChange={onChange}  optionType="button" buttonStyle="solid" style={{padding: '25px'}}/>
+
+    <Flex justify={"center"} align={"center"}>
+      {DummyData.map(plan => (
+        plan.name === "Premium"? <PricingCard isPrimary key={plan.id} PricingPlan={plan} /> : <PricingCard key={plan.id} PricingPlan={plan} />
+      ))}
+    </Flex>
+    </Flex>
+    
+
+  </div>);
+};
+
+export default LandingPage;
