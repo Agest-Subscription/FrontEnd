c12802c61e43584faee534724576b04e09493b8d
Quocc1 <caobinhoh@gmail.com>
2024-07-24 17:20:59 +0700

    Format file

--- src/constants/query.ts
+++ src/constants/query.ts
@@ -13,7 +13,6 @@ export const FEATURE = "feature";
 export const PRICING_PLANS = "pricing-plans";
 export const PRICING_PLAN = "pricing-plan";
 
-
 export const USER_ME = "user-me";
 
 export const LANDING_PAGES = "landing-pages";
